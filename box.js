92a394a2a92b20c1c8771fe59731fefd84191aaa
Sj1743 <68275266+Sj1743@users.noreply.github.com>
2020-09-12 16:30:17 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -9,40 +9,52 @@ class Box{
         this.width = 30;
         this.height = 30;
         this.colour = colour;
+        this.visiblity = 255;
         World.add(world, this.body);
       }
+
       display(){
-        var angle = this.body.angle;
+        //var fade = 255;
+        if(this.body.speed < 5){
+          var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
-        switch (this.colour) {
+        var hue = this.colour;
+        rectMode(CENTER);
+        switch (hue) {
           case 0 :
             fill(246, 189, 96);
-            rectMode(CENTER);
-            rect(0, 0, this.width, this.height);
             break;
           case 1 :
             fill(245, 202, 195);
-            rectMode(CENTER);
-            rect(0, 0, this.width, this.height);
             break;
           case 2 :
             fill(242, 132, 130);
-            rectMode(CENTER);
-            rect(0, 0, this.width, this.height);
             break;
           case 3 :
             fill(157, 129, 137);
-            rectMode(CENTER);
-            rect(0, 0, this.width, this.height);
             break;
           default :
-            fill("black")
-            rectMode(CENTER);
-            rect(0, 0, this.width, this.height);
+            fill("black");
             break;
         }
+        rect(0, 0, this.width, this.height);
         pop();
+        }else{
+          World.remove(world, this.body);
+          push();
+          this.visibility = this.visibility - 0.001;
+          pop();
+          // World.remove(world, this.body);
+          // push();
+          // translate(this.body.position.x, this.body.position.y);
+          // rotate(this.body.angle);
+          // fade = fade - 50;
+          // tint(255, 10);
+          // rect(0, 0, 30, 30);
+          // pop();
+          // console.log(fade);
+        }      
       }
 }
